972c48c951afe4245248d6a471f6fd3448f32986
ArqTras <biuro@ar-net.info>
2021-02-02 02:00:57 +0100

    Repo linking change

--- build/download-binaries.js
+++ build/download-binaries.js
@@ -4,7 +4,7 @@ const path = require("path")
 
 async function download () {
     const { platform } = process
-    const repoUrl = "https://api.github.com/repos/morelo-network/morelo/releases/latest"
+    const repoUrl = "https://api.github.com/repos/morelonetwork/morelo/releases/latest"
     try {
         const pwd = process.cwd()
         const downloadDir = path.join(pwd, "downloads")
